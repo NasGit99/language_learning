bc32ba1d99ec7d62d40a8a88a7decd3df196846f
NasGit99 <nasirthomasit@gmail.com>
2025-05-29 18:33:16 -0400

    Adding unit test for language code validation

--- tests/test_language_code_resource.py
+++ tests/test_language_code_resource.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest import mock
+from unittest.mock import patch
 import sys
 import os
 
@@ -8,7 +10,7 @@ import os
 # Add the 'src' directory to the sys.path to make all modules inside it accessible
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/language_modifier/')))
 
-from language_resource import validate_codes
+from language_code_resource import validate_codes
 
 class TestUserInput(unittest.TestCase):
 
@@ -16,6 +18,12 @@ class TestUserInput(unittest.TestCase):
         valid_language = 'English'
 
         self.assertTrue(validate_codes(valid_language))  
+
+    @patch('builtins.input', side_effect=['exit'])
+    def test_invalid_language(self, mocked_input):
+        result = validate_codes('FakeLang')
+        self.assertIsNone(result)
+    
 
 if __name__ == '__main__':
     unittest.main()
