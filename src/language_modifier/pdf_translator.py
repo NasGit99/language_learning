7e7606210c8614f1aef966f97b74c92a446a96e0
NasGit99 <nasirthomasit@gmail.com>
2025-10-07 00:24:07 -0400

    Fix an issue where text was not being properly selected

--- src/language_modifier/pdf_translator.py
+++ src/language_modifier/pdf_translator.py
@@ -1,5 +1,5 @@
 from language_modifier.base_translator import TranslatorCore
-from language_modifier.language_translator import translate_text
+from language_modifier.language_translator import translate_text,translate_bulk
 import asyncio
 import logging
 import pymupdf
@@ -21,27 +21,29 @@ class PdfTranslator(TranslatorCore):
                        "font": [],
                        "size": [],
                        "color": [],
-                       "origin":[]}
-        #TODO: Also grab page to reduce scrambling of words
-        
+                       "origin":[],
+                       "bbox":[]}
         for page in pdf:
             txt_page = page.get_textpage().extractDICT()["blocks"]
+            logging.info(page)
 
             for block in txt_page:
                 lines = block.get("lines")
                 for line in lines:
                     spans = line.get("spans")
                     for metadata in spans:
-                        pdf_content["text"].append(metadata.get("text"))
-                        pdf_content["font"].append(metadata.get("font"))
-                        pdf_content["size"].append(metadata.get("size"))
-                        pdf_content["color"].append(metadata.get("color"))
-                        pdf_content["origin"].append(metadata.get("origin"))
+                        logging.info(metadata)
+                        if metadata.get("text").strip():
+                            pdf_content["text"].append(metadata.get("text"))
+                            pdf_content["font"].append(metadata.get("font"))
+                            pdf_content["size"].append(metadata.get("size"))
+                            pdf_content["color"].append(metadata.get("color"))
+                            pdf_content["origin"].append(metadata.get("origin"))
+                            pdf_content["bbox"].append(metadata.get("bbox"))
                         logging.info(f"""Metadata is: \n Text: {pdf_content["text"]}, 
                                      \n Font: {pdf_content["font"]}, 
                                      \n Size: {pdf_content["size"]}, 
                                      \n Color:{pdf_content["color"]}
-                                     \n Origin:{pdf_content["origin"]}
                                                     """)
         return pdf, pdf_content
     
@@ -49,33 +51,33 @@ class PdfTranslator(TranslatorCore):
         pdf, pdf_content = self.pdf_text_extractor()
         self.full_output_path= self.file_exists()
 
-        for text,font,size,color,origin in zip(
-            pdf_content["text"],
+        txt_src = pdf_content["text"]
+        translation_source = "\n".join(txt_src)    
+
+        translated_content = asyncio.run(translate_text(translation_source,self.target_lang_code))
+
+        pdf_content["translations"] = translated_content.split("\n")
+
+        for translations,font,size,color,origin,bbox in zip(
+            pdf_content["translations"],
             pdf_content["font"],
             pdf_content["size"],
             pdf_content["color"],
-            pdf_content["origin"]) :
-            #TODO: Translate function is running for each line causing slow processing
-            translated_text = asyncio.run(translate_text(text, self.target_lang_code))
+            pdf_content["origin"],
+            pdf_content["bbox"]) :
 
             for page in pdf:
-                logging.info(f"Translated Line: {translated_text}")
-
-                hits = page.search_for(text) # Finding areas to replace text
-                logging.info (f"These are the hits:{hits}")
-
-                for rect in hits:
-                    page.add_redact_annot(rect)
+                page.add_redact_annot(bbox)
                 
                 font = self.font_validator(font)
                 color = self.color_validator(color)
                 #TODO: Replace this with the page from our pdf_content so we can save it correctly
                 # Removes content 
                 page.apply_redactions(images=pymupdf.PDF_REDACT_IMAGE_NONE,graphics=pymupdf.PDF_REDACT_LINE_ART_NONE) 
-                # Replaces content with correct data 
+                # # Replaces content with correct data 
                 page.insert_text(
                     origin,
-                    translated_text,
+                    translations,
                     fontname=font,
                     fontsize=size,
                     color=color,
@@ -87,8 +89,7 @@ class PdfTranslator(TranslatorCore):
 
     def font_validator(self,font_name):
             base_fonts = pymupdf.Base14_fontdict.keys()
-
-            if font_name not in base_fonts:
+            if font_name.lower() not in base_fonts:
                 logging.warning(f"Font '{font_name}' is not a Base14 font.")
                 return "Helvetica"
             else:
